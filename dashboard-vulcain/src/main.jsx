ab4565245f9c1d06cd4101a5c4e507932608cbe9
Eliandro <teles.eliandro@gmail.com>
2026-04-14 11:47:41 +0200

    Update main.jsx

--- dashboard-vulcain/src/main.jsx
+++ dashboard-vulcain/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppV4.jsx'
+import App from './AppV5.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
